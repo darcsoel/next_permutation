b13acf2a14e01a936b1cfc981f7804edb605cea6
Ross <rostikvunarchuk@gmail.com>
2021-11-02 22:57:42 +0200

    solved

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from typing import List
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
+        if len(nums) < 2:
+            return None
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        nums_count = len(nums)
+        first_decrease_element_index = None
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        for i in range(1, nums_count):
+            prev_index = nums_count - i - 1
+            cur_index = nums_count - i
+
+            if nums[prev_index] < nums[cur_index]:
+                first_decrease_element_index = prev_index
+                break
+        else:
+            # if list is sorted desc - sorting in asc
+            nums.sort()
+            return None
+
+        larger_than_first_decreased_index = None
+
+        for i in range(first_decrease_element_index, nums_count):
+            if nums[first_decrease_element_index] < nums[i]:
+                if larger_than_first_decreased_index is None or nums[i] <= nums[larger_than_first_decreased_index]:
+                    larger_than_first_decreased_index = i
+
+        nums[first_decrease_element_index], nums[larger_than_first_decreased_index] = (
+            nums[larger_than_first_decreased_index],
+            nums[first_decrease_element_index],
+        )
+
+        to_rotate = first_decrease_element_index + 1
+        nums[to_rotate:] = reversed(nums[to_rotate:])
+
+        return None
--- tests.py
+++ tests.py
@@ -1,1 +1,45 @@
 # import pytest
+
+from main import Solution
+
+
+def test_1():
+    initial = [1, 2, 3]
+    Solution().nextPermutation(initial)
+    assert initial == [1, 3, 2]
+
+
+def test_2():
+    initial = [3, 2, 1]
+    Solution().nextPermutation(initial)
+    assert initial == [1, 2, 3]
+
+
+def test_3():
+    initial = [1, 1, 5]
+    Solution().nextPermutation(initial)
+    assert initial == [1, 5, 1]
+
+
+def test_4():
+    initial = [1]
+    Solution().nextPermutation(initial)
+    assert initial == [1]
+
+
+def test_5():
+    initial = [1, 3, 2]
+    Solution().nextPermutation(initial)
+    assert initial == [2, 1, 3]
+
+
+def test_6():
+    initial = [2, 3, 1]
+    Solution().nextPermutation(initial)
+    assert initial == [3, 1, 2]
+
+
+def test_7():
+    initial = [2, 3, 1, 3, 3]
+    Solution().nextPermutation(initial)
+    assert initial == [2, 3, 3, 1, 3]
